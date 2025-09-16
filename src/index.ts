5e48de79dee202213e2aae83fc24a991dc22bd15
PLThabangR <rakgoropothabang@gmail.com>
2025-09-16 02:50:17 +0200

    Adding middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import express from "express";
+import express,{ Express } from "express";
 import  router  from "./routes/bookRoutes";
 import  authorRouter  from "./routes/authorRoutes";
 import { logger } from "./middleware/logger";
+import bodyParser from "body-parser";
 
 //create express app instance
-const app = express();
+const app:Express = express();
 //middleware to allow json
 app.use(express.json());
+//middleware to parse json body 
+app.use(bodyParser.json());
 //middleware to log requests
 app.use(logger);
 //use routes to handle requests
 app.use(router);
 app.use(authorRouter);
 
+const PORT = process.env.PORT || 3000;
 //start server
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
