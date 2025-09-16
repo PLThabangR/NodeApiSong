a4d251cdf9807481444a832138974f548540c1b0
PLThabangR <rakgoropothabang@gmail.com>
2025-09-16 03:07:35 +0200

    Adding error 404 middleware

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import  router  from "./routes/bookRoutes";
 import  authorRouter  from "./routes/authorRoutes";
 import { logger } from "./middleware/logger";
 import bodyParser from "body-parser";
+import { notFoundErrorHandler } from "./middleware/error";
 
 //create express app instance
 const app:Express = express();
@@ -12,10 +13,14 @@ app.use(express.json());
 app.use(bodyParser.json());
 //middleware to log requests
 app.use(logger);
+
+
 //use routes to handle requests
 app.use(router);
 app.use(authorRouter);
 
+///middlewarre to handle 404
+app.use(notFoundErrorHandler);
 const PORT = process.env.PORT || 3000;
 //start server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
